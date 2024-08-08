7ef3ba41943f0e4af32f7f6e2d3165d16bb5ba1d
juliodev <juliocesar8881@hotmail.com>
2024-08-07 20:01:57 -0500

    Implementación finalizada

--- src/main.js
+++ src/main.js
@@ -14,23 +14,21 @@ const api = axios.create({
         Authorization: `Bearer ${TOKEN}` 
     },
     params:{
-        'languje':'language=en-US'
+        'languje':'language=es-MX'
     }
 })
 
-async function getTrendingMoviesPreview(){
-    const {data,status} = await api('trending/movie/day')
-    const movies = data.results
-    console.log(movies)
+//Utils
 
-     //Accedo a la sección con id trendingPreview y a sus elementos con clase trendingPreview-movieList
-     trendingMoviesPreviewList.innerHTML= ''
-     
+function createMovies(movies, container){
+    container.innerHTML=''       
     movies.forEach(movie=> {        
-       
         const movieContainer = document.createElement('div') // Creo un elemento tipo div en el html
             movieContainer.classList.add('movie-container') // accedo al objeto movieContainer a su propiedad classList y le adiciono una clase
-
+            movieContainer.addEventListener('click',()=>{
+                location.hash = `#movie=${movie.id}`
+                navigator()
+            })
         const movieImg = document.createElement('img')//Creo un elemento tipo img en el html
             movieImg.classList.add('movie-img')// accedo al objeto movieImg a su propiedad classList y le adiciono una clase
             movieImg.setAttribute('alt', movie.title)//Adiciono el atributo title, el cual me llega en el elemento que estoy recorriendo
@@ -38,46 +36,117 @@ async function getTrendingMoviesPreview(){
                 'src',
                  'https://image.tmdb.org/t/p/w300' + movie.poster_path, //Concateno la url base que establece una archo de 300 pixeles con la propiedad que tiene el final de la url; el cual lo recibo en el elemento que estoy recorriendo
             )
-
             movieContainer.appendChild(movieImg);
-            trendingMoviesPreviewList.appendChild(movieContainer);
+            container.appendChild(movieContainer);
+            
     });
     
 }
 
-
-async function getCategoriesMovies(){
-    const {data,status} = await api('genre/movie/list')
-    const categories = data.genres
-    console.log(categories)
-
-
-    //Accedo a la sección con id trendingPreview y a sus elementos con clase trendingPreview-movieList
-
-    categoriesPreviewList.innerHTML = ''   
-
-     // Recorro el objeto movies, el cual contiene la respuesta del servicio
-     categories.forEach(category =>{        
+function createCategories(categories,container){
+    container.innerHTML=''
+    // Recorro el objeto movies, el cual contiene la respuesta del servicio
+    categories.forEach(category =>{        
         // Creo un elemento tipo div en el html
         const catoriesContainer = document.createElement('div')
         // accedo al objeto movieContainer a su propiedad classList y le adiciono una clase
             catoriesContainer.classList.add('category-container')
         // Creo un elemento tipo h3 en el html
         const categoriesTitle = document.createElement('h3')
-            categoriesTitle.classList.add('category-title')
+            categoriesTitle.classList.add('category-title')            
             categoriesTitle.setAttribute('id', 'id'+category.id) // Concateno la palabra id con el id que me devuelve el servicio para que me tome los estilos conforme al CSS.
-
+            categoriesTitle.addEventListener('click', ()=>{
+                location.hash= `#category=${category.id}-${category.name}`
+                categoriesPage()
+            })
         const categoriesTitleText = document.createTextNode(category.name)
-
         categoriesTitle.appendChild(categoriesTitleText)
         catoriesContainer.appendChild(categoriesTitle)
-        categoriesPreviewList.appendChild(catoriesContainer)
-
-
+        container.appendChild(catoriesContainer)
     })
 }
 
+// Llamados APIS
+
+async function getTrendingMoviesPreview(){
+    const {data,status} = await api('trending/movie/day')
+    const movies = data.results
+    console.log(movies)
+
+    createMovies(movies,trendingMoviesPreviewList) // Llamo a la función que crea la estructura HTML de forma dinámica
+}
+
+async function getMoviesByCategories(id){   
+    const {data,status} = await api('discover/movie',{
+        params:{
+            with_genres:id
+        }
+       
+    })
+    const movies = data.results
+    console.log(movies)
+
+    createMovies(movies,genericSection) // Llamo a la función que crea la estructura HTML de forma dinámica
+    
+}
+
+async function getCategoriesMovies(){
+    const {data,status} = await api('genre/movie/list')
+    const categories = data.genres
+    console.log(categories)
+
+  createCategories(categories,categoriesPreviewList)     
+}
+
+
+async function getMoviesBySearch(query){
+    const {data, status} = await api('search/movie',{
+        params:{
+            query,
+        }
+    })
+    const movies = data.results
+    console.log(movies)
+    createMovies(movies,genericSection) // Llamo a la función que crea la estructura HTML de forma dinámica
+}
  
+async function getTrendingMovies(){
+    const {data,status}= await api('trending/movie/day')
+
+    const movies = data.results
+    console.log(movies)
+
+    createMovies(movies,genericSection)
+}
+
+async function getMovieById(movie_id){
+    const {data:movie,status}= await api(`movie/${movie_id}`)   
+    console.log(movie)
+
+    const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path
+
+    headerSection.style.background = `
+    linear-gradient(
+    180deg, 
+    rgba(0, 0, 0, 0.35) 19.27%, 
+    rgba(0, 0, 0, 0) 29.17%),
+    url(${movieImgUrl})`
+
+    movieDetailTitle.textContent=movie.title
+    movieDetailDescription.textContent=movie.overview
+    movieDetailScore.textContent=movie.vote_average
+
+    createCategories(movie.genres,movieDetailCategoriesList)
+    getRelatedMoviesId(movie_id)
+   
+}
+
+async function getRelatedMoviesId(movie_id){
+    const {data} = await api(`movie/${movie_id}/recommendations`)
+    const relatedMovies = data.results
+    
+    createMovies(relatedMovies,relatedMoviesContainer) // Llamo a la función que crea la estructura HTML de forma dinámica
+}
 
 // Iniciar el servidor
 // app.listen(port, () => {
--- src/navigation.js
+++ src/navigation.js
@@ -1,6 +1,6 @@
 
 searchFormBtn.addEventListener('click', ()=>{
-    location.hash='#search='
+    location.hash='#search='+searchFormInput.value
     navigator()
 })
 
@@ -9,27 +9,45 @@ trendingBtn.addEventListener('click', ()=>{
     navigator()
 })
 
-arrowBtn.addEventListener('click', ()=>{
-    location.hash= 'Home'
+arrowBtn.addEventListener('click', () => {   
+   // location.hash = window.history.back()
+   location.hash='#home'
     navigator()
-})
+   
+});
 
 //Llamo la función navigator
 window.addEventListener('DOMContentLoaded',navigator,false)
-window.addEventListener('haschange',navigator,false)
+window.addEventListener('haschange',navigator,false) // Actualizo el contenido sin recargar completamente el sitio
 
 
 
 function navigator(){
-console.log({location})
+    console.log({location})
+    // if anidado utilizando operadores ternarios en donde pregunto a location.hash empieza de cierta forma
 
-// if anidado utilizando operadores ternarios en donde pregunto a location.hash empieza de cierta forma
-location.hash.startsWith('#trends')  ? trendsPages():
-location.hash.startsWith('#search=') ? searchPages():
-location.hash.startsWith('#movie=')  ? moviesDatailsPages():
-location.hash.startsWith('#category=') ? categoriesPage():
-homePage()
+    if(location.hash.startsWith('#trends')){
+        trendsPages()
+    }else if(location.hash.startsWith('#search=')){
+        searchPages()
+    }else if(location.hash.startsWith('#movie=')){
+        moviesDatailsPages()
+    }else if(location.hash.startsWith('#category=')){
+        categoriesPage()
+    }else{
+        homePage()
+    }
+    // location.hash.startsWith('#trends')  ? trendsPages():
+    // location.hash.startsWith('#search=') ? searchPages():
+    // location.hash.startsWith('#movie=')  ? moviesDatailsPages():
+    // location.hash.startsWith('#category=') ? categoriesPage():
+    // homePage()
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    
 }
+
+
 
 function homePage(){
     console.log('Home')
@@ -47,9 +65,12 @@ function homePage(){
     genericSection.classList.add('inactive')
     movieDetailSection.classList.add('inactive')
 
-
-    getTrendingMoviesPreview()
-    getCategoriesMovies()
+    const childrentrendingMoviesPreviewList = Array.from(trendingMoviesPreviewList.children) // Declaro array con el número de hijos de la sección de trending
+    // Valido si la longitud del array cambia para no volver a llamar a las funciones que se conecta con las apis
+    if(!childrentrendingMoviesPreviewList.length) {
+        getTrendingMoviesPreview()
+        getCategoriesMovies()
+    } 
 }
 
 function categoriesPage(){
@@ -67,6 +88,20 @@ function categoriesPage(){
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.remove('inactive')
     movieDetailSection.classList.add('inactive')
+
+    //Creo un nuevo array utilizando el metodo split en donde determino como separador de cada nuevo elemento el simbolo igual(=)
+
+    const [_,categoryData] = location.hash.split('=') // ['#category','id-name'] Obtengo solo el elemento en la posición dos del array
+    const [categoryId,categoryName] = categoryData.split('-')
+    const categoryName2 = decodeURI(categoryName) //Decodifico el nombre de la pelicula para que deje de reemplazar el espacio por %20
+    
+    //Remplazo el titulo de la categoria dinamicamente
+    headerCategoryTitle.innerHTML=categoryName2
+
+   
+        getMoviesByCategories(categoryId)
+    
+    
 }
 
 function moviesDatailsPages(){
@@ -84,6 +119,10 @@ function moviesDatailsPages(){
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.add('inactive')
     movieDetailSection.classList.remove('inactive')
+
+     // [movie_movieId]    
+    const [_,movieId] = location.hash.split('=')
+    getMovieById(movieId)
 }
 
 function searchPages(){
@@ -94,13 +133,17 @@ function searchPages(){
     arrowBtn.classList.remove('inactive')
     arrowBtn.classList.remove('header-arrow--white')
     headerTitle.classList.add('inactive')
-    headerCategoryTitle.classList.remove('inactive')
+    headerCategoryTitle.classList.add('inactive')
     searchForm.classList.remove('inactive')
 
     trendingPreviewSection.classList.add('inactive')
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.remove('inactive')
     movieDetailSection.classList.add('inactive')
+
+    const [_,query]= location.hash.split('=')    
+    getMoviesBySearch(query)
+    
 }
 
 function trendsPages(){
@@ -118,4 +161,11 @@ function trendsPages(){
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.remove('inactive')
     movieDetailSection.classList.add('inactive')
+   
+    headerCategoryTitle.innerHTML ='Tendencias'
+
+    getTrendingMovies()
 }
+
+
+
